5e3e74cfc12a275522b29674a83f104bcc39e952
Lucas Cabral <lucascabral239@gmail.com>
2022-12-13 11:05:41 -0300

    Resolvido o conflito de echo

--- index.php
+++ index.php
@@ -3,11 +3,6 @@
 <<<<<<< HEAD
 echo "Olá mundo 456!";
 
-echo "Olá Udemy!"
-=======
-echo "Olá mundo 123!";
->>>>>>> 1a348e218e0fa0e54f8dc53d381aea13ed21aeae
-
-echo "Olá Hcoda"
+echo "Olá Udemy e Hcode!"
 
 ?>
